e049737c1284fa228d5361d4fc62a0261b1577c3
doktorj7 <doktorjeff@Jeff-T-Doktors-iMac.local>
2014-03-03 14:49:51 -0700

    Updated sample for SDK 4.1.5

--- Cue Point Usage/Pods/Pods-environment.h
+++ Cue Point Usage/Pods/Pods-environment.h
@@ -10,23 +10,23 @@
 #define COCOAPODS_POD_AVAILABLE_Brightcove_Player_SDK
 #define COCOAPODS_VERSION_MAJOR_Brightcove_Player_SDK 4
 #define COCOAPODS_VERSION_MINOR_Brightcove_Player_SDK 1
-#define COCOAPODS_VERSION_PATCH_Brightcove_Player_SDK 4
+#define COCOAPODS_VERSION_PATCH_Brightcove_Player_SDK 5
 
 // ReactiveCocoa
 #define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
 #define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
 #define COCOAPODS_VERSION_MINOR_ReactiveCocoa 2
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 4
 
 // ReactiveCocoa/Core
 #define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
 #define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
 #define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 2
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 4
 
 // ReactiveCocoa/no-arc
 #define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
 #define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
 #define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 2
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 4
 
